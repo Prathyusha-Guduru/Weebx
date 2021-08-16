23936682b78d1bf39152c6723cdde5c9bc581028
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-08-16 20:31:46 +0530

    Updated API call for topANime

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from "react"
 import "../src/assets/scss/main.scss"
 
+const topAnimeURL = "https://api.jikan.moe/v3/top/anime/1/bypopularity"
+const getTopAnime = async() => {
+	const response = await (await fetch(topAnimeURL));
+  const data = await response.json();
+  const topAnime = data.top.slice(0, 10);
+  return topAnime;
+}
+
+
+getTopAnime()
+  .then(topAnime => {
+    console.log(topAnime);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+
+
 function App() {
+  
   return (
     <div className="App">
+      
       <h1>Hello, world!</h1>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Header from './components/Header';
 import App from './App';
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
+   
   </React.StrictMode>,
   document.getElementById('root')
 );
