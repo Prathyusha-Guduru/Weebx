e4e2b5652ebc0b66c974db747146fdacd01b24b6
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-08-20 20:41:59 +0530

    Updated search anime api calls

--- src/App.js
+++ src/App.js
@@ -2,24 +2,14 @@ import React from "react"
 import "../src/assets/scss/main.scss"
 
 const topAnimeURL = "https://api.jikan.moe/v3/top/anime/1/bypopularity"
+
+
 const getTopAnime = async() => {
 	const response = await (await fetch(topAnimeURL));
   const data = await response.json();
   const topAnime = data.top.slice(0, 10);
   return topAnime;
 }
-
-
-// getTopAnime()
-//   .then(topAnime => {
-//     console.log(topAnime);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
-
-const searchAnimeURL = "https://api.jikan.moe/v3/search/anime?q=Grand%20Blue&page=1"
-
 
 const settings = {
   method: 'POST',
@@ -29,22 +19,12 @@ const settings = {
   }
 };
 
-const searchAnime = async() => {
-	const response = await (await fetch(searchAnimeURL));
+const searchAnime = async(animeName) => {
+	const response = await (await fetch(`https://api.jikan.moe/v3/search/anime?q=${animeName}$order_by=title&sort-asc&limit=15`));
   const data = await response.json();
   console.log(data.results);
   return response;
 }
-
-console.log(searchAnime());
-// getTopAnime()
-//   .then(topAnime => {
-//     console.log(topAnime);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
-
 
 function App() {
   
