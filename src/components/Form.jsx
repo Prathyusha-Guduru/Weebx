3975b4a97d67fa19b16e59e84e3a1246245e12db
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-08-21 01:07:20 +0530

    Updated status state

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,22 +10,72 @@ const searchAnime = async(animeName) => {
 
 const Form = () => {
 	const [inputText,setInputText] = useState("");
+	const [status,setStatus] = useState("default");
+	const [genreAnime, setGenreAnime] = useState([])
+
+	const genreDict = {
+		"default" : 11,
+		"action" : 1,
+			"romance" : 22,
+			"ecchi" : 9,
+			"fantasy" : 10
+	}
+
+	const getGenreAnime = (genre) => {
+		const num = genreDict[genre]
+		console.log('num',num);
+			fetch(`https://api.jikan.moe/v3/genre/anime/${num}/1`)
+			.then(
+				response=>response.json()
+			).then(
+				(data)=>{
+						console.log(data.anime)
+						setGenreAnime(data.anime)
+				}
+			).catch(
+				err => console.log(err)
+			)
+	}
+
+
+	useEffect(()=>{
+		getGenreAnime(status)
+	},[status])
+
 	const inputTextHandler = (e) => {
 		setInputText(e.target.value)
 	}
+
 	const searchHandler  = (e) => {
 		e.preventDefault();
 		console.log(inputText);
 		searchAnime(inputText);
 	}
+
+	const statusHandler = (e) => {
+		if (e.target.value === "all"){
+			setStatus('default')
+		}else{
+			setStatus(e.target.value)
+		}
+}
+
 	return (
 		<form action="">
 			<input type="text" name="search" id="" onChange = {inputTextHandler}/>
 			<button type="submit" onClick = {searchHandler}>Search</button>
+			<div className="select">
+		 <select name="genre" className="filter-genre" onChange = {statusHandler}>
+						<option value="all">All</option>
+						<option value="action">Action</option>
+						<option value="romance">Romance</option>
+						<option value="ecchi">ecchi</option>
+						<option value="fantasy">Fantasy</option>
+			</select>
+		</div>
 		</form>
 	)	
-		
-	
+
 }
 
 export default Form;
